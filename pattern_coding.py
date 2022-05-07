bec560f0acbc98746db5c242e8f5642edc8385ba
vsr73 <104710083+vsr73@users.noreply.github.com>
2022-05-08 00:00:45 +0530

    first change

---
+++ pattern_coding.py
@@ -0,0 +1,97 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:/Users/Dell/git_tut/patterncoding.py ===============
+enter no of rows for all the 10 patterns
+            and #NOTE:10 th pattern only works with the 5 rows:-5
+
+1st pattern
+
+*
+**
+***
+****
+*****
+
+2nd pattern
+
+*****
+****
+***
+**
+*
+
+3rd pattern
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+4th pattern
+
+     **
+    *  *
+   **********
+  *      *
+ *        *
+5th pattern
+A A A A A 
+B B B B B 
+C C C C C 
+D D D D D 
+E E E E E 
+
+6th pattern
+
+A B C D E 
+A B C D E 
+A B C D E 
+A B C D E 
+A B C D E 
+
+7th pattern
+
+     
+     A 
+    A B 
+   A B C 
+  A B C D 
+ 
+8th pattern
+
+    A 
+   A B C 
+  A B C D E 
+ A B C D E F G 
+A B C D E F G H I 
+
+9th pattern
+
+    A 
+   A B C 
+  A B C D E 
+ A B C D E F G 
+A B C D E F G H I 
+ A B C D E F G 
+  A B C D E 
+   A B C 
+    A 
+     
+
+10th pattern
+
+ABCDEFGHI
+ ABCDEFG
+  ABCDE
+   ABC
+    A
+   ABC
+  ABCDE
+ ABCDEFG
+ABCDEFGHI
